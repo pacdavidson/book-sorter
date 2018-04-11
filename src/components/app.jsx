52328f961ee812d596807a245b70ca4bc2529cec
Patrick Davidson <padavidson@lbl.gov>
2018-04-11 14:17:12 -0700

    add store.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import '../base-styles/index.scss'
+
+const store = configureStore({})
 
 export default class App extends Component {
 
     render () {
         return (
-            <div>
-                <p>Hello World!</p>
-            </div>
+            <Provider store={ store }>
+                <div>
+                    <p>Hello World!</p>
+                </div>
+            </Provider>
         )
     }
 }
